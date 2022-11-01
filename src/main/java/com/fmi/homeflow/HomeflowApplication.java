0a241bf03a2619f2f73ab741e3ddef435769e03f
BogdanDinescu <bogdandinescu@tutanota.com>
2022-11-01 19:27:01 +0200

    Initial commit

---
+++ src/main/java/com/fmi/homeflow/HomeflowApplication.java
@@ -0,0 +1,13 @@
+package com.fmi.homeflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeflowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fmi/homeflow/HomeflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmi.homeflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
