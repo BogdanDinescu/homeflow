908e17150594cb7019216f6d27d170593cab8b53
Alecsandu <alecsandu678@gmail.com>
2022-12-17 18:30:30 +0200

    info: Fixed issue with security deps.

--- src/main/java/com/fmi/homeflow/service/AuthenticationService.java
+++ src/main/java/com/fmi/homeflow/service/AuthenticationService.java
@@ -8,7 +8,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -21,7 +20,7 @@ public class AuthenticationService implements UserDetailsService{
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username) {
         return userRepository.findByUsername(username)
                 .map(user -> org.springframework.security.core.userdetails.User
                         .withUsername(user.getUsername())
